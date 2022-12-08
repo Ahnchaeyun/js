3c44151d0a749b7e13c4d503bae3731c280dc93f
Ahnchaeyun <202130119@email.daelim.ac.kr>
2022-12-08 14:35:26 +0900

    클릭 이름 변경

--- js/example.js
+++ js/example.js
@@ -1,1 +1,22 @@
-alert('?');
+const nameH1Elemnent = document.querySelector('h1.inline');
+const connectNameElemnent = document.querySelector('span.red');
+
+const localName = localStorage.getItem('name');
+if (localName) {
+  nameH1Elemnent.textContent = localName;
+  connectNameElemnent.textContent = localName;
+}
+
+nameH1Elemnent.onclick = () => {
+  alert('클릭되었습니다.');
+  const inputName = prompt('이름을 입력해 주세요.');
+  if (inputName) {
+    localStorage.setItem('name', inputName);
+
+    nameH1Elemnent.textContent = inputName;
+    connectNameElemnent.textContent = inputName;
+  } else {
+    alert('이름이 입력되지 않았습니다.');
+  }
+};
+/***/
